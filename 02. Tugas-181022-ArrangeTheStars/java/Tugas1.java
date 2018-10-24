d99e84afdfd33fb3b45b24b24323c370096b1d96
setyaisme <setya.bagoes@gmail.com>
2018-10-24 11:31:51 +0700

    Latihan 1

---
+++ 02. Tugas-181022-ArrangeTheStars/java/Tugas1.java
@@ -0,0 +1,5 @@
+class Tugas1 {
+    public static void main(String[] args) {
+        System.out.println("***\n***\n***\n***");
+    }
+}
---
+++ 02. Tugas-181022-ArrangeTheStars/java/Tugas3.java
@@ -0,0 +1,5 @@
+class Tugas3 {
+    public static void main(String[] args) {
+        System.out.println("  *  \n  *  \n *** \n * * \n*****");
+    }
+}
